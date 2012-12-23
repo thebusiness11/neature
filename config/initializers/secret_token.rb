c1a73db7a6ef78061896a1013aecf13826d61cdb
Lance Velasco <lance@mlvgroup.com>
2012-12-23 12:54:32 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Neature::Application.config.secret_token = 'c55f17df215738bbab1908c639de8d456eced80f056ab1cad9376cfc4f12210a189abce0a7e519a5cbcc9082dca449d9289cc30bbcb44924f736f7d628f93bea'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Neature::Application.config.session_store :cookie_store, key: '_neature_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Neature::Application.config.session_store :active_record_store
